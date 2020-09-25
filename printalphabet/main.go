38ce3cf2bf55bbea73375414b54ea966d89cc590
MOLIVAR <maxime.olivar@ynov.com>
2020-09-25 11:09:27 +0200

    quete 1 partie 1

---
+++ printalphabet/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+
+	var aRune string = "abcdefghijklmnopqrstuvwxyz"
+	for i := 0; i < 26; i++ {
+
+		z01.PrintRune(rune(aRune[i]))
+	}
+
+	z01.PrintRune('\n')
+
+}
